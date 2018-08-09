2ec8ceb44abbd17ba026ebbc55c9fdf9e31559db
黄炜 <hw1@meitu.com>
2018-08-09 11:25:15 +0800

    添加拍照返回功能

--- GPUImageDemo/GPUImageDemo/ViewController.mm
+++ GPUImageDemo/GPUImageDemo/ViewController.mm
@@ -14,11 +14,15 @@
 #import "GPUFilter.h"
 
 @interface ViewController ()
-@property (strong, nonatomic) IBOutlet UIView *mUIView;
-@property (strong, nonatomic) GPUImageStillCamera *mCamera;
-@property (strong, nonatomic) GPUImageView *mGPUImageView;
+@property (strong, nonatomic) IBOutlet UIButton *captureButton;//拍照按钮
+@property (strong, nonatomic) IBOutlet UIView *mUIView;//装GPUImageView的容器
+@property (strong, nonatomic) GPUImageStillCamera *mCamera; //相机
+@property (strong, nonatomic) GPUImageView *mGPUImageView; //GPUImageView
 
-@property (strong, nonatomic) GPUFilter *filter;
+@property (strong, nonatomic) GPUFilter *filter; //滤镜
+
+@property BOOL isRealTime; //当前是否在实时预览 YES:实时预览  NO:拍后
+
 @end
 
 @implementation ViewController
@@ -29,6 +33,7 @@
     //初始化相机
     _mCamera = [[GPUImageStillCamera alloc] initWithSessionPreset:AVCaptureSessionPresetHigh cameraPosition:AVCaptureDevicePositionBack];
     _mCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    _isRealTime = YES;
     
     //初始化GPUImageView
     _mGPUImageView = [[GPUImageView alloc] initWithFrame:_mUIView.frame];
@@ -56,19 +61,26 @@
 
 //拍照
 - (IBAction)takePhoto:(id)sender {
-
-    
-    __weak typeof(self) weakSelf = self;
-    [_mCamera capturePhotoAsPNGProcessedUpToFilter:_filter withCompletionHandler:^(NSData *processedPNG, NSError *error) {
-        UIImage* image = [[UIImage alloc] initWithData:processedPNG];
-        if(image != nil){
-            [weakSelf.mCamera stopCameraCapture];
-            GPUImagePicture* picture = [[GPUImagePicture alloc] initWithImage:image];
-            [picture addTarget:weakSelf.filter];
-            [picture useNextFrameForImageCapture];
-            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
-        }
-    }];
+    if (_isRealTime) {
+        __weak typeof(self) weakSelf = self;
+        [_mCamera capturePhotoAsPNGProcessedUpToFilter:_filter withCompletionHandler:^(NSData *processedPNG, NSError *error) {
+            UIImage* image = [[UIImage alloc] initWithData:processedPNG];
+            if(image != nil){
+                [weakSelf.mCamera stopCameraCapture];
+                weakSelf.isRealTime = NO;
+                weakSelf.captureButton.titleLabel.text = @"返回";
+                GPUImagePicture* picture = [[GPUImagePicture alloc] initWithImage:image];
+                [picture addTarget:weakSelf.filter];
+                [picture useNextFrameForImageCapture];
+                UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+            }
+        }];
+    }
+    else {
+        [_mCamera startCameraCapture];
+        _isRealTime = YES;
+        _captureButton.titleLabel.text = @"拍照";
+    }
 }
 
 
